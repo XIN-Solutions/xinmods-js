18cf5ab864fcee53d0e6e26382b1a29af45bf473
Marnix Kok <marnix@xinsolutions.co.nz>
2020-09-11 00:02:42 +1200

    Added quality parameter

--- src/Image.js
+++ src/Image.js
@@ -5,12 +5,12 @@
 	 | || | | | | | (_| | (_| |  __/ | |_) | |_| | | | (_| |  __/ |
 	|___|_| |_| |_|\__,_|\__, |\___| |____/ \__,_|_|_|\__,_|\___|_|
 	                     |___/
-	                     
+
      Purpose:
-        
+
         To interpret image information and apply asset modification instructions to them.
         Things that can be done to them currently:
-        
+
             * crop
             * scale (x, y, or both)
             * filters: greyscale, brighter, darker
@@ -21,27 +21,27 @@ class Image {
 	hippo;
 	info;
 	operations;
-	
+
 	constructor(hippo, info) {
 		this.hippo = hippo;
 		this.info = info;
 		this.operations = [];
 	}
-	
+
 	clone() {
 		return new Image(this.hippo, this.info);
 	}
-	
+
 	reset() {
 		this.operations = [];
 		return this;
 	}
-	
+
 	greyscale() {
 		this.operations.push("filter=greyscale");
 		return this;
 	}
-	
+
 	brighter(times = 1) {
 		for (let idx = 0; idx < times; ++idx) {
 			this.operations.push('filter=brighter');
@@ -55,7 +55,12 @@ class Image {
 		}
 		return this;
 	}
-	
+
+	quality(qParam = 0.8) {
+	    this.operations.push(`quality=${qParam}`);
+	    return this;
+    }
+
 	scaleWidth(width) {
 		this.operations.push(`scale=${width},_`);
 		return this;
@@ -65,23 +70,23 @@ class Image {
 		this.operations.push(`scale=_,${height}`);
 		return this;
 	}
-	
+
 	crop(width, height) {
 		this.operations.push(`crop=${width},${height}`);
 		return this;
 	}
 
-	
+
 	toUrl() {
 		// don't do anything to the image?
 		if (this.operations.length === 0) {
 			return `${this.hippo.host}${this.hippo.options.assetPath}${this.info.path}`
 		}
-		
+
 		const opsStr = this.operations.join("/");
 		return `${this.hippo.host}${this.hippo.options.assetModPath}/${opsStr}/binaries${this.info.path}`;
 	}
-	
+
 }
 
 module.exports = Image;
